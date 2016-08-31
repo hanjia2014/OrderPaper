e070e1c7225976662c2b9cca96384d74d4071cf4
Datacom <Datacom@CALLUMG-LT002>
2016-08-31 12:37:24 +1200

    update base component

--- src/OrderPaper/wwwroot/app/components/base.component.ts
+++ src/OrderPaper/wwwroot/app/components/base.component.ts
@@ -6,6 +6,7 @@ export class BaseComponent implements AfterViewInit, ITogglable {
     IsNumberedList: boolean;
     isExpand: boolean;
     spinner: Spinner = new Spinner({ radius: 10 });
+    ListIndex: number;
 
     constructor() {
         this.IsNumberedList = true;
@@ -13,25 +14,77 @@ export class BaseComponent implements AfterViewInit, ITogglable {
     public SortableConfig() {
         var eleId = '#' + this.SortableListId;
 
-        (function ($: any) {
+        (($: any) => {
             var panelList = $(eleId);
-
             panelList.sortable({
                 // Only make the .panel-heading child elements support dragging.
                 // Omit this to make then entire <li>...</li> draggable.
                 placeholder: "sortable-hightlight",
                 handle: '.panel-heading',
-                update: function () {
+                
+                start: function (e: any, ui: any) {
+                    //Before all other events
+                    //Only occurs once each time sorting begins
+                    ui.placeholder.height(ui.item.height());
+                },
+                activate: function (e, ui) {
+                    //After the start event and before all other events
+                    //Occurs once for each connected list each time sorting begins
+                },
+                change: function (e, ui) {
+                    //After start/active but before over/sort/out events
+                    //Occurs every time the item position changes
+                    //Does not occur when item is outside of a connected list
+                },
+                over: function (e, ui) {
+                    //After change but before sort/out events
+                    //Occurs while the item is hovering over a connected list
+                },
+                sort: function (e, ui) {
+                    //After over but before out event
+                    //Occurs during sorting
+                    //Does not matter if the item is outside of a connected list or not
+                },
+                out: function (e, ui) {
+                    //This one is unique
+                    //After sort event before all drop/ending events unless **see NOTE
+                    //Occurs, only once, the moment an item leaves a connected list
+                    //NOTE: Occurs again when the item is dropped/sorting stops 
+                    //--> EVEN IF the item never left the list
+                    //--> Called just before the stop event but after all other ending events
+                },
+                beforeStop: function (e, ui) {
+                    //Before all other ending events: update,remove,receive,deactivate,stop
+                    //Occurs only once at the last possible moment before sorting stops
+                },
+                remove: function (e, ui) {
+                    //After beforeStop and before all other ending events
+                    //Occurs only once when an item is removed from a list
+                },
+                receive: function (e, ui) {
+                    //After remove and before all other ending events
+                    //Occurs only once when an item is added to a list
+                },
+                update: () => {
+                    //After receive and before all other ending events
+                    //Occurs when the DOM changes for each connected list
+                    //This can fire twice because two lists can change (remove from one
+                    //list but add to another)
                     $('.panel', panelList).each((index: any, elem: any) => {
-                        var $listItem = $(elem),
-                            newIndex = $listItem.index();
-
+                        var $listItem = $(elem), newIndex = $listItem.index();
+                        this.ListIndex = newIndex;
                         // Persist the new indices.
                     });
                 },
-                start: function (e: any, ui: any) {
-                    ui.placeholder.height(ui.item.height());
+                deactivate: function (e, ui) {
+                    //After all other events but before out (kinda) and stop
+                    //Occurs once for each connected list each time sorting ends
+                },
+                stop: function (e, ui) {
+                    //After all other events
+                    //Occurs only once when sorting ends
                 }
+
             });
         })(jQuery);
     }
