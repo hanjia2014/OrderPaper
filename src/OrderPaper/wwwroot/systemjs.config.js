bd8cba106f2520f4b1e3f9670521b57436410c0c
Datacom <Datacom@CALLUMG-LT002>
2016-08-31 16:28:02 +1200

    changed systemjs.config

--- src/OrderPaper/wwwroot/systemjs.config.js
+++ src/OrderPaper/wwwroot/systemjs.config.js
@@ -9,15 +9,25 @@
         '@angular': 'node_modules/@angular',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs': 'node_modules/rxjs',
-        'ts': 'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
-        //'ts': 'node_modules/plugin-typescript/lib/plugin.js',
-        'typescript': 'node_modules/typescript/lib/typescript.js',
+        'ts': 'node_modules/plugin-typescript/lib',
+        'typescript': 'node_modules/typescript/lib',
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'components/bootstrap.ts', defaultExtension: 'ts' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+        "ts": {
+            "main": "plugin.js"
+        },
+        "typescript": {
+            "main": "typescript.js",
+            "meta": {
+                "typescript.js": {
+                    "exports": "ts"
+                }
+            }
+        }
     };
     var ngPackageNames = [
       'common',
