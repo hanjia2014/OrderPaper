02d06efcf06a56995fc7350d4302ea0cbbedae82
Datacom <Datacom@CALLUMG-LT002>
2016-09-06 12:30:00 +1200

    side menu div

--- src/OrderPaper/wwwroot/app/components/master.component.ts
+++ src/OrderPaper/wwwroot/app/components/master.component.ts
@@ -3,8 +3,33 @@ import { Router } from '@angular/router';
 
 @Component({
     selector: 'app',
-    template: `<router-outlet></router-outlet>`,
-    styles: [],
+    template: `<!--<div class="side">
+                <div class="side_inner">
+                    <a rel="nofollow" class="Sharer_link u-button u-facebook">
+                        <i class="Sharer_icon fa fa-facebook"></i>
+                        <span class="Sharer_label">Facebook</span>
+                    </a>
+                    <a rel="nofollow" class="Sharer_link u-button u-linkedin">
+                        <i class="Sharer_icon fa fa-linkedin"></i>
+                        <span class="Sharer_label">LinkedIn</span>
+                    </a>
+                    <a rel="nofollow" class="Sharer_link u-button u-twitter">
+                        <i class="Sharer_icon fa fa-twitter"></i>
+                        <span class="Sharer_label">Twitter</span>
+                    </a>
+                </div>
+               </div>-->
+               <router-outlet></router-outlet>
+                `,
+    styles: [`
+            .side {
+                position: fixed;
+                margin: 0;
+                top: 40%;
+                width: 48px;
+                overflow: hidden;
+                left: 0;
+            }`],
     providers: []
 })
 export class MasterComponent implements OnInit {
