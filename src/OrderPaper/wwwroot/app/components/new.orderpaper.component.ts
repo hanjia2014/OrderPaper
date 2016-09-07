020980284951b075ad830c7a391b88d930d8822f
Datacom <Datacom@CALLUMG-LT002>
2016-09-07 16:06:10 +1200

    tick top menu

--- src/OrderPaper/wwwroot/app/components/new.orderpaper.component.ts
+++ src/OrderPaper/wwwroot/app/components/new.orderpaper.component.ts
@@ -11,9 +11,9 @@ import { OrderType }                                                        from
     selector: 'new-order-paper',
     template: `<h1>{{id}}</h1>
                 <div id="spinner"></div>
-                <tabs>
-                    <tab [title]="'Details'">
-                        <div class="form">
+                <div class="navbar-fixed-top container" style="box-shadow: 0 14px 24px -14px gray;">
+                    <tabs>
+                        <tab [title]="'Details'">
                             <div class="row">
                                 <div class="col-md-2">
                                     Order Paper For:
@@ -43,28 +43,29 @@ import { OrderType }                                                        from
                                     <input [(ngModel)] = "orderPaper.SittingHours" placeholder="Sitting Hours" /><br/>
                                 </div>
                             </div>
-                        
-                            <br/>
-                            <ol type="1" id="{{SortableListId}}" class="list-sortable">
-                                <li class="panel panel-info" *ngFor="let orderType of orderPaper.OrderTypes; let i = index">
-                                    <div class="panel-heading"></div>
-                                    <div class="panel-body">
-                                        <order-type [orderType] = "orderType" [index]="i"></order-type>
-                                    </div>
-                                </li>
-                            </ol>
-
-                            <a class="btn btn-lg save-button" (click)="save($event)">
-                                <span class="glyphicon glyphicon-floppy-disk"></span> Save
-                            </a>
-                        </div>
-                    </tab>
-                    <tab [title]="'Preview'">
-                        <div class="form">
-                            <a class="btn">Save</a>
-                        </div>
-                    </tab>
-                </tabs>
+                        </tab>
+                        <tab [title]="'Preview'">
+                            <div class="form">
+                                <a class="btn">Save</a>
+                            </div>
+                        </tab>
+                    </tabs>
+                </div>
+                <br/>
+                <div class="form" style="margin-top:120px">
+                    <ol type="1" id="{{SortableListId}}" class="list-sortable">
+                        <li class="panel panel-info" *ngFor="let orderType of orderPaper.OrderTypes; let i = index">
+                            <div class="panel-heading"></div>
+                            <div class="panel-body">
+                                <order-type [orderType] = "orderType" [index]="i"></order-type>
+                            </div>
+                        </li>
+                    </ol>
+                
+                    <a class="btn btn-lg save-button" (click)="save($event)">
+                        <span class="glyphicon glyphicon-floppy-disk"></span> Save
+                    </a>
+                </div>        
                 `,
     styles: [],
     providers: [OrderPaperService]
