410fa8eb0e6382a97405551cbeea010d4436c774
Datacom <Datacom@CALLUMG-LT002>
2016-09-14 13:25:14 +1200

    make it editable

--- src/OrderPaper/wwwroot/app/components/sections/bill.section.component.ts
+++ src/OrderPaper/wwwroot/app/components/sections/bill.section.component.ts
@@ -4,13 +4,39 @@ import { BillSection } from '../../models/section';
 @Component({
     selector: 'bill-section',
     template: `<h1>Bill -- {{bill.Sequence}}</h1>
-                <div>
-                    Title: {{bill.Title}}<br/>
-                    Speeches: {{bill.Speeches}}<br/>
-                    Member: {{bill.Member}}<br/>
-                    Speeches: {{bill.Speeches}}<br/>
-                    Latest event: {{bill.LatestEvent}}
-                </div>`,
+                <div class="row">
+                    <div class="col-md-2">
+                        Title:
+                    </div>
+                    <div class="col-md-8">
+                        <input [(ngModel)] = "bill.Title" placeholder="Title" />
+                    </div>
+                </div>    
+                <div class="row">
+                    <div class="col-md-2">
+                        Speeches:
+                    </div>
+                    <div class="col-md-8">
+                        <input [(ngModel)] = "bill.Speeches" placeholder="Speeches" />
+                    </div>
+                </div>   
+                <div class="row">
+                    <div class="col-md-2">
+                        Member:
+                    </div>
+                    <div class="col-md-8">
+                        <input [(ngModel)] = "bill.Member" placeholder="Member" />
+                    </div>
+                </div>   
+                <div class="row">
+                    <div class="col-md-2">
+                        Latest event:
+                    </div>
+                    <div class="col-md-8">
+                        <input [(ngModel)] = "bill.LatestEvent" placeholder="Latest Event" />
+                    </div>
+                </div>
+                `,
     styles: [],
     providers: []
 })
