6f79b8e2a71a255d71764dcdc4a198bab06e804e
Datacom <Datacom@CALLUMG-LT002>
2016-09-15 13:39:46 +1200

    disable <input> to be draggable

--- src/OrderPaper/wwwroot/app/components/sections/bill.section.component.ts
+++ src/OrderPaper/wwwroot/app/components/sections/bill.section.component.ts
@@ -1,46 +1,49 @@
-﻿import { Component, OnInit, ViewChild, Input } from '@angular/core';
+﻿import { Component, OnInit, ViewChild, Input, AfterViewInit } from '@angular/core';
 import { BillSection } from '../../models/section';
 
 @Component({
     selector: 'bill-section',
-    template: `<h1>Bill -- {{bill.Sequence}}</h1>
-                <div class="row">
-                    <div class="col-md-2">
-                        Title:
-                    </div>
-                    <div class="col-md-8">
-                        <input [(ngModel)] = "bill.Title" placeholder="Title" />
-                    </div>
-                </div>    
-                <div class="row">
-                    <div class="col-md-2">
-                        Speeches:
-                    </div>
-                    <div class="col-md-8">
-                        <input [(ngModel)] = "bill.Speeches" placeholder="Speeches" />
-                    </div>
-                </div>   
-                <div class="row">
-                    <div class="col-md-2">
-                        Member:
-                    </div>
-                    <div class="col-md-8">
-                        <input [(ngModel)] = "bill.Member" placeholder="Member" />
-                    </div>
-                </div>   
-                <div class="row">
-                    <div class="col-md-2">
-                        Latest event:
-                    </div>
-                    <div class="col-md-8">
-                        <input [(ngModel)] = "bill.LatestEvent" placeholder="Latest Event" />
-                    </div>
+    template: `
+                <div class="undraggable">
+                   <h1>Bill -- {{bill.Sequence}}</h1>
+                   <div class="row">
+                       <div class="col-md-2">
+                           Title:
+                       </div>
+                       <div class="col-md-8">
+                           <input class="undraggable" [(ngModel)] = "bill.Title" placeholder="Title" />
+                       </div>
+                   </div>    
+                   <div class="row">
+                       <div class="col-md-2">
+                           Speeches:
+                       </div>
+                       <div class="col-md-8">
+                           <input class="undraggable" [(ngModel)] = "bill.Speeches" placeholder="Speeches" />
+                       </div>
+                   </div>   
+                   <div class="row">
+                       <div class="col-md-2">
+                           Member:
+                       </div>
+                       <div class="col-md-8">
+                           <input class="undraggable" [(ngModel)] = "bill.Member" placeholder="Member" />
+                       </div>
+                   </div>   
+                   <div class="row">
+                       <div class="col-md-2">
+                           Latest event:
+                       </div>
+                       <div class="col-md-8">
+                           <input class="undraggable" [(ngModel)] = "bill.LatestEvent" placeholder="Latest Event" />
+                       </div>
+                   </div>
                 </div>
                 `,
     styles: [],
     providers: []
 })
-export class BillSectionComponent implements OnInit {
+export class BillSectionComponent implements OnInit, AfterViewInit {
     @Input()
     bill: BillSection;
     @Input()
@@ -50,5 +53,15 @@ export class BillSectionComponent implements OnInit {
     }
     ngOnInit() {
         var m = this.bill;
+    }
+
+    ngAfterViewInit() {
+        $('.undraggable')
+            .on('focus', function (e) {
+                $('.panel').attr("draggable", false);
+            })
+            .on('blur', function (e) {
+                $('.panel').attr("draggable", true);
+            });
     }
 }
